cc059e74e0f27d84c7972c2ed4c386ff64334931
erika-goodwin <shellvoerika@gmail.com>
2021-04-12 11:56:50 -0700

    a

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,7 +17,7 @@ $('document').ready(function () {
             $('nav').removeClass('sticky');
         }
     }, {
-        offset: '60px'
+        offset: '-5px'
     })
     
     ////* FOR SCROLL ON BUTTONS */////
@@ -111,12 +111,64 @@ $('document').ready(function () {
       );
     
     
-    ////* MOBILE NAVIGATION */////    
-    $('js--nav-icon').click(function){
-        var nav = $('js--main-nav');
+    ////* MOBILE NAVIGATION */////        
+//      $('.js--nav-icon').click(function(){
+//        const nav = $('.js--main-nav')
+//        const icon = $('.js--nav-icon ion-icon')
+       
+//        nav.slideToggle(200/*mm/sec*/)
+ 
         
-        nav.slideToggle(200/*mm/sec*/);  
-    }
+//       if (icon[0].name == 'menu-outline') {
+//            icon[0].name = 'close-outline'
+//        } else if (icon[0].name == 'close-outline') {
+//           icon[0].name = 'menu-outline'
+//       }
+    //       }) 
+    
+    
+    $('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element) {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+      
+        //Gets the class name of the element that triggered the event
+        var clicked = element.target.className;
+      
+        //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+        if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black')
+          return;
+      
+        //Opens and closes the menu
+        if ($(window).width() < 768) {
+          nav.slideToggle(200);
+        }
+      
+        //Changes icon states of the menu button
+        if (icon.hasClass('ion-navicon-round')) {
+          icon.addClass('ion-close-round');
+          icon.removeClass('ion-navicon-round');
+        } else {
+          icon.addClass('ion-navicon-round');
+          icon.removeClass('ion-close-round');
+        }
+      });
+      
+      
+      $(window).resize(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+      
+        if ($(window).width() > 767) {
+          nav.css("display", "block");
+          icon.addClass('ion-close-round');
+          icon.removeClass('ion-navicon-round');
+        } else {
+          nav.css("display", "none");
+          icon.addClass('ion-navicon-round');
+          icon.removeClass('ion-close-round');
+        }
+      
+      });
     
     
 });
